e4c529d88b76a2a27282443f04981075b1c72fda
Shota Kudo <kudou-s0camfci76w@outlook.com>
2024-01-10 10:07:54 +0900

    Add support for variadic arguments in Lambda

--- src/core/types/lambda.rs
+++ src/core/types/lambda.rs
@@ -1,15 +1,27 @@
 /* core/types/lambda.rs */
 
+use std::borrow::Cow;
 use std::cell::RefCell;
 use std::rc::Rc;
 
 use crate::core::environment::Environment;
 use crate::core::eval::eval;
 use crate::core::types::error::arity_error;
+use crate::core::types::error::arity_error_min;
+use crate::core::types::error::Error;
 use crate::core::types::error::Result;
 use crate::core::types::function::Function;
+use crate::core::types::meta::Meta;
 use crate::core::types::symbol::Symbol;
 use crate::core::value::Value;
+
+pub const SYMBOL_ANPERSAND: Symbol = Symbol {
+    name: Cow::Borrowed("&"),
+    meta: Meta {
+        doc: Cow::Borrowed(""),
+        mutable: false,
+    },
+};
 
 #[derive(Debug, Clone)]
 pub struct Lambda {
@@ -21,15 +33,48 @@ pub struct Lambda {
 // TODO: &rest
 impl Function for Lambda {
     fn call(&self, args: Vec<Value>) -> Result<Value> {
-        if args.len() != self.args.len() {
-            return Err(arity_error(self.args.len(), args.len()));
-        }
-
         let local_env = Environment::new_local_environment(self.environment.clone());
         let mut ast = Vec::<Value>::new();
 
-        for (sym, val) in self.args.iter().zip(args) {
-            local_env.borrow_mut().insert_to_current(sym.clone(), val)?;
+        let argc = self.args.len();
+
+        let mut exist_rest = false;
+
+        if argc >= 2 {
+            if self.args[argc - 2] == SYMBOL_ANPERSAND {
+                exist_rest = true;
+            }
+            let rest_symbol = &self.args[argc - 1];
+            local_env
+                .borrow_mut()
+                .insert_to_current(rest_symbol.clone(), Value::Nil)?;
+        }
+
+        if exist_rest && args.len() < argc - 2 {
+            return Err(arity_error_min(argc - 2, args.len()));
+        }
+
+        if !exist_rest && args.len() != self.args.len() {
+            return Err(arity_error(self.args.len(), args.len()));
+        }
+
+        for (i, arg) in args.iter().enumerate() {
+            let sym = &self.args[i];
+            if sym == &SYMBOL_ANPERSAND {
+                if !exist_rest {
+                    return Err(Error::Type(format!(
+                        "invalid argument: {}",
+                        sym.to_string()
+                    )));
+                }
+                let rest_sym = &self.args[i + 1];
+                let rest_args = Value::as_vector(args[i..].to_vec())?;
+                local_env.borrow_mut().set(&rest_sym, rest_args)?;
+                break;
+            }
+            let _ = local_env
+                .borrow_mut()
+                .insert_to_current(sym.clone(), arg.clone());
         }
 
         let mut result = Value::Nil;
